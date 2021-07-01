2692f515d2ad1877e7781c7bbbb148e8b3de5031
EKE00372 <eke00372@gmail.com>
2021-07-01 21:37:50 +0800

    add BC dungeon table

---
+++ BCDungeonTools/Dungeons/ManaTombs.lua
@@ -0,0 +1,8 @@
+local MDT = MDT
+local L = MDT.L
+local dungeonIndex = 7
+MDT.mapInfo[dungeonIndex] = {};
+--MDT.scaleMultiplier[dungeonIndex] = 1.2
+MDT.dungeonTotalCount[dungeonIndex] = {normal=1,teeming=1000,teemingEnabled=true}
+--MDT.mapPOIs[dungeonIndex] = {};
+MDT.dungeonEnemies[dungeonIndex] = {};
